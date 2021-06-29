679b37fcd14242b7dbc6204fb93367651c85ab55
Grégoire Chevalier <aagchevalier@gmail.com>
2021-06-29 13:39:33 +0200

    2021-06-29 # 3 : Setup de la page d'accueil

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 2102
+
+app.get('/', (req, res) => {
+  res.send('APIngpong !')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
